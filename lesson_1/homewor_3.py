b1c21d841737863b7a2aa92fb57b153fb6e2b0ba
Mshine1993 <mixalis_93@mial.ru>
2021-07-06 15:05:12 +0300

    ??

--- lesson_1/homewor_3.py
+++
@@ -1,5 +0,0 @@
-n = input('Введите число: ')
-
-sum = (int(n)) + (int(n+n)) + (int(n+n+n))
-
-print (sum)
--- lesson_1/homework_2.py
+++
@@ -1,8 +0,0 @@
-seconds = int(input('Введите количество секунд от 1 до 86400: '))
-hour = (seconds // 3600)
-time_sec = (seconds % 60)
-min = ((seconds - (hour*3600)) // 60)
-
-time = (hour, min, time_sec)
-
-print (time)
